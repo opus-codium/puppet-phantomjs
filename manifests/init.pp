a6022407c7b962ad2ea3e8d81b2a33a6e8e0972c
Romain Tartière <romain@blogreen.org>
2016-06-28 16:46:43 +0200

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,69 @@
+# Manage phantomjs installation
+#
+# Old package build instructions from
+# https://github.com/aeberhardo/phantomjs-linux-armv6l
+# FIXME: In testing.
+class phantomjs (
+  $version = '1.9.8'
+) {
+  if $::architecture == 'armv7l' {
+    $basedir = '/home/deploy/phantomjs'
+    $pixman_baseurl = 'http://www.opensource.apple.com/source/X11libs/X11libs-60/pixman/pixman-0.20.2/pixman/'
+
+    vcsrepo { $basedir:
+      ensure   => present,
+      provider => 'git',
+      source   => 'https://github.com/ariya/phantomjs.git',
+      revision => $version,
+      depth    => 1,
+      user     => 'deploy',
+      notify   => Exec['phantomjs-build'],
+    }
+
+    file { "${basedir}/src/qt/src/3rdparty/pixman":
+      ensure  => directory,
+      owner   => 'deploy',
+      group   => 'deploy',
+      mode    => '0755',
+      require => Vcsrepo[$basedir],
+    }
+
+    exec { 'fetch pixman-arm-neon-asm.h':
+      command => "/usr/bin/curl -O ${basedir}/src/qt/src/3rdparty/pixman/pixman-arm-neon-asm.h ${pixman_baseurl}/pixman-arm-neon-asm.h?txt",
+      creates => "${basedir}/src/qt/src/3rdparty/pixman/pixman-arm-neon-asm.h",
+      require => File["${basedir}/src/qt/src/3rdparty/pixman"],
+    }
+
+    exec { 'fetch pixman-arm-neon-asm.S':
+      command => "/usr/bin/curl -O ${basedir}/src/qt/src/3rdparty/pixman/pixman-arm-neon-asm.h ${pixman_baseurl}/pixman-arm-neon-asm.S?txt",
+      creates => "${basedir}/src/qt/src/3rdparty/pixman/pixman-arm-neon-asm.S",
+      require => File["${basedir}/src/qt/src/3rdparty/pixman"],
+    }
+
+    exec { 'phantomjs-build':
+      cwd         => '/home/deploy/phantomjs',
+      path        => '/bin:/usr/bin:/usr/local/bin',
+      command     => './build.sh --confirm',
+      timeout     => 0,
+      refreshonly => true,
+      require     => [Vcsrepo[$basedir], Exec['fetch pixman-arm-neon-asm.h'], Exec['fetch pixman-arm-neon-asm.S']],
+      notify      => Exec['phantomjs-install'],
+    }
+
+    exec { 'phantomjs-install':
+      command => '/usr/bin/install -o root -g root -m 755 /home/deploy/phantomjs/bin/phantomjs /usr/bin',
+      require => Exec['phantomjs-build'],
+      creates => '/usr/bin/phantomjs',
+    }
+  } elsif $::architecture == 'amd64' {
+    file { '/usr/local/bin/phantomjs':
+      ensure => file,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+      source => 'puppet:///modules/phantomjs/phantomjs-1.9.8-linux-x86_64',
+    }
+  } else {
+    fail "Architecture (${::architecture}) not supported"
+  }
+}
